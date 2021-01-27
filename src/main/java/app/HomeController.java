a6ffb98d4771b61d4463caf7127f53ffd02206db
Volodymyr Novak <novakvova@gmail.com>
2021-01-27 18:39:49 +0200

    Add rest controller

---
+++ src/main/java/app/HomeController.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String home() {
+        return "Hello World--!";
+    }
+}
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,19 +2,13 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
 @SpringBootApplication
 public class Main {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
