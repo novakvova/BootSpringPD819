bee7a1382ff964416aebbb5fa9922c983eec0093
Volodymyr Novak <novakvova@gmail.com>
2021-02-11 18:29:39 +0200

    The best upload file

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -19,8 +19,8 @@ public class Main {
     @Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
+//            storageService.deleteAll();
+            //storageService.init();
         };
     }
 
